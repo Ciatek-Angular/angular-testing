519e25b4c4a0d52c1362dc251ede286b45bb4439
ahmadyassinkhoja <ahmad.mohammad.khoja@gmail.com>
2018-11-07 11:54:35 +0200

    testing solution

--- testing/src/app/exercise/exercise.component.ts
+++ testing/src/app/exercise/exercise.component.ts
@@ -13,43 +13,49 @@ export class ExerciseComponent implements OnInit {
   }
 
   add(x,y){
-   
+    return x+ y
   }
 
   subtract(x,y){
-    
+    return x-y
   }
 
   multiply(x,y,z){
-   
+    return x * y * z
   }
 
   divide(x,y){
-   
+    return x / y
   }
 
   removeLastItemInArray(numbers){
-    
+    numbers.pop()
+    return numbers
   }
 
   removeSlashes(path){
-    
+    let name = path.split('/')
+    return name[2]
   }
 
   removeItemFromArray(vegetables){
-    
+    vegetables.splice(2,1)
+    return vegetables 
   }
   
   addItemsToArray(vegetables){
-    
+    vegetables.splice(2,0,'cucumber','carrot')
+    return vegetables 
   }
 
   add2Messages(m1,m2){
-   
+    let m3 = m1.concat(m2)
+    return m3
   }
 
   convertArrayToString(fruits){
-   
+    let result = fruits.join(",")
+    return result
   }
 
 
